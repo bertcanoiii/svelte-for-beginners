8e1ddf1b536aca49e51c0dde675849216394fbe7
Bert Cano <bertcanoiii@gmail.com>
2022-07-26 08:58:50 -0700

    refactor form and post into components

---
+++ src/components/Form.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let submitForm;
+    let author = '';
+    let post = '';
+
+    function sendSubmitForm(){
+        submitForm(author, post);
+        author = '';
+        post = '';
+    }
+</script>
+
+<h1 class="font-bold">Add Post</h1>
+<div class="form-control w-80 items-center">
+    <label for="author" class="label font-bold text-2xl">Author</label>
+    <input bind:value={author} id="author" name="author" type="text" placeholder="Author"
+           class="input input-bordered w-full max-w-xs"/>
+    <label for="post" class="label">
+        <span class="label font-bold text-m">Your Post</span>
+    </label>
+    <textarea bind:value={post} id="post" class="textarea textarea-bordered h-24 w-full"
+              placeholder="Post"></textarea>
+    <button on:click={sendSubmitForm} class="btn mt-6">Submit</button>
+</div>
---
+++ src/components/Post.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let author;
+    export let post;
+</script>
+
+
+<div class="card w-96 bg-base-100 shadow-xl">
+    <div class="card-body items-start">
+        <h2 class="card-title">{author}</h2>
+        <p>{post}</p>
+    </div>
+</div>
